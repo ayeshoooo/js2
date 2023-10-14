0ba03de0664f6d587a1a7520ff1e9b50fde897b0
Ayesha khan <112780976+ayeshoooo@users.noreply.github.com>
2023-10-14 23:46:37 +0500

    Add files via upload

---
+++ newFile.js
@@ -0,0 +1,120 @@
+
+//practice question
+
+//Question 2.1
+
+//let str1 = 'Laurence';
+//let str2 = "Svekis";
+//let val1 = undefined;
+//let val2 = null;
+//let myNum = 1000;
+
+//console.log("str1", typeof str1);
+//console.log("str2", typeof str2);
+//console.log("val1", typeof val1);
+//console.log("val2", typeof val2);
+//console.log("myNum", typeof myNum);
+
+
+
+ //Question 2.2
+
+//  let name = 'Ayesha';
+//  let age = 19;
+//  let canCode = true;
+
+//  console.log('My name is',name);
+//  console.log('I am', age, ' year old')
+//  console.log('Can I code Javascript:',canCode)
+
+ //Question 2.4
+ 
+
+//  let a = 10;
+//  let b = 20;
+//  let c = 30;
+
+ //Add b to a
+
+//  b+= a;
+//  console.log(b);
+
+ //Divide a by c
+//  c/=a;
+//  console.log(c);
+
+ //• Replace the value of c with the modulus of c and b
+
+//  c%=b;
+//  console.log(c);
+
+// Print all three numbers to the console
+// console.log("a:", a);
+// console.log("b:", b);
+// console.log("c:", c);
+
+
+
+
+// Define the value in miles
+//let miles = 130;
+
+// Convert miles to kilometers using the conversion factor
+//let kilometers = miles * 1.60934;
+
+// Log the value in kilometers in the specified formaconsole.log(`The distance of ${miles} miles is equal to ${kilometers.toFixed(4)} kilometers`);
+
+
+//BMI CALCULATOR
+
+// let heightInInches = 70;
+// let weightInPounds = 150;
+
+// Convert height from inches to centimeters using the conversion factor
+//let heightInCentimeters = heightInInches * 2.54;
+
+// Convert weight from pounds to kilograms using the conversion factor
+//let weightInKilograms = weightInPounds * 0.453592;
+
+// Output the results to the console
+// console.log(`Height: ${heightInInches} inches is equal to ${heightInCentimeters} centimeters.`);
+// console.log(`Weight: ${weightInPounds} pounds is equal to ${weightInKilograms} kilograms.`);
+
+
+
+
+
+// Define values for height in inches and weight in pounds
+// let heightInInches = 70;
+// let weightInPounds = 150;
+
+// Convert height from inches to centimeters using the conversion factor
+//let heightInCentimeters = heightInInches * 2.54;
+
+// Convert weight from pounds to kilograms using the conversion factor
+//let weightInKilograms = weightInPounds * 0.453592;
+
+// Output the results to the console
+// console.log(`Height: ${heightInInches} inches is equal to ${heightInCentimeters} centimeters.`);
+// console.log(`Weight: ${weightInPounds} pounds is equal to ${weightInKilograms} kilograms.`);
+
+// Additional conversion: Convert weight from kilograms to pounds
+// let weightInKilos = 68; // Example weight in kilograms
+// let weightInPoundsFromKilos = weightInKilos * 2.2046;
+
+// console.log(`Weight: ${weightInKilos} kilograms is equal to ${weightInPoundsFromKilos} pounds.`);
+
+
+//Practice exercise 2.3
+
+// let a=2;
+// let b=3;
+
+// prompt('value of a:');
+// prompt('value of b:');
+
+ 
+
+// console.log(a**2+b**2);
+
+ 
